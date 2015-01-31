993b54b505ac7e78ea904263f738525b17d07328
taintech <rinattainov@gmail.com>
2015-01-31 10:10:27 +0200

    Initial commit

---
+++ src/main/java/com/taintech/timus/Sum.java
@@ -0,0 +1,19 @@
+package com.taintech.timus;
+
+import java.io.*;
+import java.util.*;
+
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        out.println(a + b);
+
+        out.flush();
+    }
+}
